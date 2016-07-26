f04a25e05daa127fb7e0f3812602caae93be203c
janos <janoshoener@gmail.com>
2016-07-26 18:36:55 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+# setup(
+#     name = "rtg_graph_generator",
+#     version = "0.1",
+#     author = "Janos Hoener",
+#     author_email = "janoshoener@gmail.com",
+#     description = ("A Python Port of the RTG generator implemented by Leman Akoglu in Matlab"),
+#     keywords = "rtg graph genrator",
+#     url = "https://github.com/jim-pansn/rtg",
+#     py_modules=['rtg'],
+#     install_requires=[
+#           'numpy==1.11.0'
+#       ]
+# )       
+setup(name='rtg',
+      version='1.0',
+      py_modules=['rtg'],
+      )
